8caa2ac1bdf8b2a4faaeded10a0f6632c00eaa70
MartinFrank77 <martin.frank@irix.ch>
2025-11-25 17:22:53 +0100

    Cors issues

--- src/main/java/com/github/martinfrank/elitegames/adventureserver/server/security/SecurityConfig.java
+++ src/main/java/com/github/martinfrank/elitegames/adventureserver/server/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.github.martinfrank.elitegames.adventureserver.server.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,17 +8,15 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -74,12 +71,7 @@ public class SecurityConfig {
         return cfg.getAuthenticationManager();
     }
 
-
     private List<String> allowedOrigins() {
-        String allowed = """
-                https://elitegames.v6.rocks/adventure-game,
-                https://localhost:8080/adventure-game-api
-                """;
-        return Arrays.stream(allowed.split(",")).map(String::trim).toList();
+        return List.of("https://elitegames.v6.rocks/adventure-game");
     }
 }
