02ad42ca7320aa53cec3df8bdf18320ce80dc7df
MartinFrank77 <martin.frank@irix.ch>
2025-11-26 10:23:34 +0100

    zwischenstand umgebungsvariablen

--- src/main/java/com/github/martinfrank/elitegames/adventureserver/server/security/SecurityConfig.java
+++ src/main/java/com/github/martinfrank/elitegames/adventureserver/server/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.github.martinfrank.elitegames.adventureserver.server.security;
 
+import com.github.martinfrank.elitegames.adventureserver.server.game.GameController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,15 +21,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
 
-//    @Value("${security.cors.allowedOrigins}")
-//    private String allowedOrigins;
+    @Value("${security.cors.allowedOrigins}")
+    private String allowedOrigins = "";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
 
 
     @Bean
@@ -72,6 +81,8 @@ public class SecurityConfig {
     }
 
     private List<String> allowedOrigins() {
-        return List.of("https://elitegames.v6.rocks");
+        List<String> theList = Arrays.stream(allowedOrigins.split(",")).map(String::trim).toList();
+        LOGGER.info("the list: {}", theList);
+        return theList;
     }
 }
